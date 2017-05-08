d39bdd189c324e730cd11f1f5a374bcbdecacd54
conanskyforcce <conanskyforce@163.com>
2017-05-08 11:59:11 -0400

    index

--- index.js
+++ index.js
@@ -7,5 +7,4 @@ handle["/"] = requestHandlers.start;
 handle["/start"] = requestHandlers.start;
 handle["/upload"] = requestHandlers.upload;
 handle["/show"] = requestHandlers.show;
-
 server.start(router.route,handle);
